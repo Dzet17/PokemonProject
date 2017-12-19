6c75d99a8751b30ea42efa99f52c794af4e3c4f2
Dylan Zettler <dylanzettler@gmail.com>
2017-12-19 07:41:09 -0700

    Added more constructors and getters and setters to PokemonPanel

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,34 @@
 package pokemon.view;
 
-public class PokemonFrame
-{
+import javax.swing.JFrame;
 
+import pokemon.controller.PokemonController;
+import pokemon.view.PokemonPanel;
+
+public class PokemonFrame extends JFrame
+{
+	private PokemonController appController;
+	private PokemonPanel appPanel;
+	
+	public Pokemon(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		appPanel = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setSize(500,500);
+		this.setTitle("Pokemon");
+		this.setContentPane(appPanel);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+
+	public Object getBaseController()
+	{
+		return false;
+	}
 }
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,61 @@
 package pokemon.view;
 
-public class PokemonPanel
-{
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
+import pokemon.controller.PokemonController;
+
+public class PokemonPanel extends JPanel
+	{
+		private PokemonController appController;
+		private JButton pokemonButton;
+		private JTextField inputField;
+		private JTextArea pokemonArea;
+		private SpringLayout baseLayout;
+		private SpringLayout baseLayout_1;
+		private JButton checkerButton;
+		private JLabel infoLabel;
+		private JScrollPane chatScrollPane;
+	
+
+	public PokemonPanel(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		pokemonButton = new JButton("Enter");
+		inputField = new JTextField();
+		infoLabel = new JLabel("Enter Pokemon");
+		baseLayout = new SpringLayout();
+		
+		setupScrollPane();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupScrollPane()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
 }
